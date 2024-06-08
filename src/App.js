d58b7ecfe85d088cd5761612b018b6f9dab969bc
sreepriya73 <sreepriyaillikkatt@gmail.com>
2024-06-08 14:19:42 +0530

    cpmtd

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import AddPhoto from './component/AddPhoto';
+import SearchPhoto from './component/SearchPhoto';
+import DeletePhoto from './component/DeletePhoto';
+import ViewPhoto from './component/ViewPhoto';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<AddPhoto/>}/>
+      <Route path='/search' element={<SearchPhoto/>}/>
+      <Route path='/delete' element={<DeletePhoto/>}/>
+      <Route path='/view' element={<ViewPhoto/>}/>
+    </Routes>
+    </BrowserRouter>
 
-export default App;
+  )
+    
+  
+}
+  
+export default App
+
